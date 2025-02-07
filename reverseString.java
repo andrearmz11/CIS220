8b8444af3d4331763da8da00af17c4cd51b88301
andrearmz11 <anddyrmzrgl>
2025-02-07 08:13:59 -0700

    Added reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,7 +1,12 @@
-import java.util.Scanner;
+//Andrea Ramirez
+//Chpt 2 PA
 
+// This program reverses a string using recursion and outputs the reversed string.
 public class reverseString {
-
+	
+/* This method reverses a string by moving the first character to the end,
+   and then calling itself on the rest of the string. */
+   
      public static String reverseString (String word) {
 		if (word.length( ) <= 1) {
 			return word;
@@ -13,12 +18,12 @@ public class reverseString {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		String input, result;
-		
+		//Ask for and get the string to be reversed 
 		System.out.print("Enter the string to be reversed: ");
 		input = scnr.nextLine();
 		
 		result = reverseString(input);
-		
+		//Output the reversed string
 		System.out.printf("Reversed: %s\n", result);
 	}
 }
